33fadb9ec15553be3ccd859040f774a2c91b896e
jigar <jigarbhoye04@gmail.com>
2025-02-14 23:17:39 +0530

    feat: Implement dynamic routes
    -Implemented user, auth, and subscription routes

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 import express from 'express';
 import { PORT } from './config/env.js';
+import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.routes.js';
+import subscriptionRouter from './routes/subscriptions.routes.js';
+
 
 const app = express();
+
+app.use('/api/v1/auth',authRouter);
+app.use('/api/v1/users',userRouter);
+app.use('/api/v1/subscriptions',subscriptionRouter);
+
 
 // Routes
 app.get('/',(req,res) =>{
---
+++ routes/user.routes.js
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+
+const userRouter = Router();
+
+//GET /users -> get all users
+//GET /users/:id -> get a user by id    //users/123
+
+userRouter.get('/', (req, res) => res.send({ title: 'GET all users' }));
+
+userRouter.get('/:id', (req, res) => res.send({ title: 'GET user details' })); //:id = dyanmaic parameter
+
+userRouter.post('/', (req, res) => res.send({ title: 'CREATE new user' }));
+
+userRouter.put('/:id', (req, res) => res.send({ title: 'UPDATE user' }));
+
+userRouter.delete('/:id', (req, res) => res.send({ title: 'DELETE user' }));
+
+export default userRouter;
