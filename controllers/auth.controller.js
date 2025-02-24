7f8aed8930e6d7111496ca95bd1d899e264cba05
jigar <jigarbhoye04@gmail.com>
2025-02-24 11:16:26 +0530

    feat: implement signIn - with error handling - JWT token generation

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -52,6 +52,43 @@ export const signUp = async (req, res, next) => {
    }
 };
 
-export const signIn = async (req, res, next) => {};
+export const signIn = async (req, res, next) => {
+   try {
+      const { email, password } = req.body;
+
+      const user = await User.findOne({ email });
+      if (!user) {
+         const error = new Error("User not found");
+         error.statusCode = 404;
+         throw error;
+      }
+
+      const isValidPassword = await bcrypt.compare(password, user.password);
+      if (!isValidPassword) {
+         const error = new Error("Invalid username or password"); // for extra security, we can use the same error message for both cases
+         error.statusCode = 401; //unauthorized
+         throw error;
+      }
+
+      const token = jwt.sign(
+         {
+            userId: user._id,
+         },
+         JWT_SECRET,
+         { expiresIn: JWT_EXPIRES_IN }
+      );
+
+      res.status(200).json({
+         success: true,
+         message: "User logged in successfully",
+         data: {
+            token,
+            user,
+         },
+      });
+   } catch (error) {
+      next(error);
+   }
+};
 
 export const signOut = async (req, res, next) => {};
